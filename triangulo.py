5fe4123454c10ef1ad3d5b1b8cc06d90e211bfe8
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-10-16 15:50:29 -0300

    commit inicial

---
+++ triangulo.py
@@ -0,0 +1,8 @@
+n1 = float(input('\033[0;49;31mdigite um comprimento\n'))
+n2 = float(input('\033[0;49;33mdigite um outro comprimento\n'))
+n3 = float(input('\033[0;49;35mdigite um outro comprimento\n'))
+
+if n1 < n2+n3 and n2 < n1+n3 and n3 < n1+n2:
+    print('\033[0;49;34mForma um triangulo')
+else:
+    print('\033[0;49;39mNão forma um triangulo')
