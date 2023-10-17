2055ec4332d7a498ed1d19ee926902ceeb6b9d35
vinicius vieira <148118982+arnold-vinicius@users.noreply.github.com>
2023-10-17 13:32:12 -0300

    Update triangulo.py

--- triangulo.py
+++ triangulo.py
@@ -6,3 +6,5 @@ if n1 < n2+n3 and n2 < n1+n3 and n3 < n1+n2:
     print('\033[0;49;34mForma um triangulo')
 else:
     print('\033[0;49;39mNão forma um triangulo')
+
+# eita breia ai sim seu dpimenta
